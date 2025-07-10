dfb97a40dcb023f4e055630abe7b60f495ee14dc
Farjana Eti <etimourakkhieti@gmail.com>
2025-07-10 23:34:39 +0600

    right menu design on

--- src/app/profile/[id]/page.jsx
+++ src/app/profile/[id]/page.jsx
@@ -1,7 +1,26 @@
+import Feed from '@/component/homeitems/feed'
+import LeftMenu from '@/component/homeitems/leftMenu'
+import RightMenu from '@/component/homeitems/rightMenu'
 import React from 'react'
 
 export default function ProfilePage() {
   return (
-    <div>Profile</div>
+   <div className="flex gap-6 pt-6">
+         {/* left */}
+        <div className="hidden xl:block w-[20%] border">
+       <LeftMenu></LeftMenu>
+        </div>
+        {/* center */}
+        <div className="w-full lg:w-[70%] xl:w-[50%] border">
+       <div className="flex flex-col gap-4">
+        
+        <Feed></Feed>
+       </div>
+        </div>
+        {/* right */}
+        <div className="hidden lg:block w-[30%] border">
+         <RightMenu user="test"></RightMenu>
+         </div>
+       </div>
   )
 }
--- src/component/homeitems/rightMenu.jsx
+++ src/component/homeitems/rightMenu.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 import FriendRequest from '../rightNenuComponent/friendRequest'
 import Birthday from '../rightNenuComponent/birthday'
 import Ad from '../rightNenuComponent/ad'
+import UserInfo from '../rightNenuComponent/userInfo'
+import UserMedia from '../rightNenuComponent/userMedia'
 
 export default function RightMenu({user}) {
   return (
     <div>
+       {user? (
+        <>
+        <UserInfo user={user}></UserInfo>
+        <UserMedia user={user}></UserMedia>
+        </>
+      ):null}
       <FriendRequest></FriendRequest>
       <Birthday ></Birthday>
      <Ad size="md" />
